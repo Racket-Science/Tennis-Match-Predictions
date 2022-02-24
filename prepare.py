7ccdc7b708772c810807385fafe43f136c688fb0
masonsherbondy <sherbondy.mason@gmail.com>
2022-02-23 20:03:32 -0600

    more prep

--- prepare.py
+++ prepare.py
@@ -18,15 +18,21 @@ def prepare_atp():
     # create target variable 
     df['player_1_wins'] = np.where(df['winner'] == df['player_1_name'], True, False)
 
-    # create dummy columns for surface, level, hand, and round 
+    #  create dummy columns for surface, level, hand, and round 
     dummy_df = pd.get_dummies(df[['surface', 'tourney_level', 'player_1_hand', 'player_2_hand', 'round']], dummy_na=False, drop_first=[False])
-    # concat dummy columns to df
+    # oncat dummy columns to df
     df = pd.concat([df, dummy_df], axis=1)
     
 
     # drop all walkovers (no useful stats) and best of 1 matches (extremely rare format)
     df = df.drop(df[df.score == 'W/O'].index)
     df = df.drop(df[df.best_of == 1].index)
+
+    # set dataframe to years 1999-2019 (21 years of recent data minus corona years)
+    df = df['1999-01-01':'2020-01-01']
+    # drop janky records
+    df = df[df['player_1_first_serve_%'].notnull()]
+    df = df[df['player_2_first_serve_%'].notnull()]
 
     df_clean = df = df.dropna(subset=['player_1_aces'])
     
@@ -40,3 +46,52 @@ def split_data(df):
     train, validate = train_test_split(train, test_size = .3, random_state = 123)
     
     return train, validate, test
+
+def calculate_column_nulls(df):
+
+    '''
+    This function  defines one parameter, a dataframe, and returns a dataframe that holds data regarding null values and ratios (pertaining to the whole column) 
+    in the original frame.
+    '''   
+    
+    output = []    # set an empty list
+    df_columns = df.columns.to_list()   # gather columns
+
+    for column in df_columns:   # commence for-loop
+        missing = df[column].isnull().sum()    # assign variable to number of rows that have null values
+        ratio = missing / len(df)   # assign variable to ratio of rows with null values to overall rows in column
+        # assign a dictionary for your dataframe to accept       
+        r_dict = {'nulls': missing,
+                  'null_ratio': round(ratio, 5),
+                  'null_percentage': f'{round(ratio * 100, 2)}%'
+                 }
+        output.append(r_dict)   # add dictonaries to list
+
+    column_nulls = pd.DataFrame(output, index = df_columns)    # design frame
+    column_nulls = column_nulls.sort_values('nulls', ascending = False)
+
+    return column_nulls
+
+def calculate_row_nulls(df):
+
+    '''
+    This function  defines one parameter, a dataframe, and returns a dataframe that holds data regarding null values and ratios (pertaining to the whole row) 
+    in the original frame.
+    '''   
+    
+    output = []    # create an empty list
+    nulls = df.isnull().sum(axis = 1)   # gather values in a series
+    for n in range(len(nulls)):    # commence 4 loop
+        missing = nulls[n]     # assign variable to nulls
+        ratio = missing / len(df.columns)   # assign variable to ratio
+        # assign a dictionary for your dataframe to accept
+        r_dict = {'nulls': missing,
+                  'null_ratio': round(ratio, 5),
+                  'null_percentage': f'{round(ratio * 100)}%'
+                 }
+        output.append(r_dict)   # add dictonaries to list
+
+    row_nulls = pd.DataFrame(output, index = df.index)    # design frame
+    row_nulls = row_nulls.sort_values('nulls', ascending = False)
+
+    return row_nulls    # return the dataframe
