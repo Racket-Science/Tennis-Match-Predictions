125d90e1f708ce302dac41e44bdf4ae6529a9ab0
masonsherbondy <sherbondy.mason@gmail.com>
2022-02-23 20:04:45 -0600

    m

---
+++ Notebooks/prepare.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def prepare_atp():
+
+    # read .csv into a dataframe
+    df = pd.read_csv('ATPMain.csv', index_col = 0)
+
+    # set index to the tournament start date
+    df = df.set_index('tourney_date')
+
+    # convert index to datetime object and sort in ascending order
+    df.index = pd.to_datetime(df.index, format = '%Y%m%d')
+    df = df.sort_index(ascending = True)
+
+    # create target variable 
+    df['player_1_wins'] = np.where(df['winner'] == df['player_1_name'], True, False)
+
+    #  create dummy columns for surface, level, hand, and round 
+    dummy_df = pd.get_dummies(df[['surface', 'tourney_level', 'player_1_hand', 'player_2_hand', 'round']], dummy_na=False, drop_first=[False])
+    # oncat dummy columns to df
+    df = pd.concat([df, dummy_df], axis=1)
+    
+
+    # drop all walkovers (no useful stats) and best of 1 matches (extremely rare format)
+    df = df.drop(df[df.score == 'W/O'].index)
+    df = df.drop(df[df.best_of == 1].index)
+
+    # set dataframe to years 1999-2019 (21 years of recent data minus corona years)
+    df = df['1999-01-01':'2020-01-01']
+    # drop janky records
+    df = df[df['player_1_first_serve_%'].notnull()]
+    df = df[df['player_2_first_serve_%'].notnull()]
+
+    df_clean = df = df.dropna(subset=['player_1_aces'])
+    
+    return df
+
+def split_data(df):
+    '''
+    Takes in a dataframe and returns train, validate, and test subset dataframes. 
+    '''
+    train, test = train_test_split(df, test_size = .2, random_state = 123)
+    train, validate = train_test_split(train, test_size = .3, random_state = 123)
+    
+    return train, validate, test
+
+def calculate_column_nulls(df):
+
+    '''
+    This function  defines one parameter, a dataframe, and returns a dataframe that holds data regarding null values and ratios (pertaining to the whole column) 
+    in the original frame.
+    '''   
+    
+    output = []    # set an empty list
+    df_columns = df.columns.to_list()   # gather columns
+
+    for column in df_columns:   # commence for-loop
+        missing = df[column].isnull().sum()    # assign variable to number of rows that have null values
+        ratio = missing / len(df)   # assign variable to ratio of rows with null values to overall rows in column
+        # assign a dictionary for your dataframe to accept       
+        r_dict = {'nulls': missing,
+                  'null_ratio': round(ratio, 5),
+                  'null_percentage': f'{round(ratio * 100, 2)}%'
+                 }
+        output.append(r_dict)   # add dictonaries to list
+
+    column_nulls = pd.DataFrame(output, index = df_columns)    # design frame
+    column_nulls = column_nulls.sort_values('nulls', ascending = False)
+
+    return column_nulls
+
+def calculate_row_nulls(df):
+
+    '''
+    This function  defines one parameter, a dataframe, and returns a dataframe that holds data regarding null values and ratios (pertaining to the whole row) 
+    in the original frame.
+    '''   
+    
+    output = []    # create an empty list
+    nulls = df.isnull().sum(axis = 1)   # gather values in a series
+    for n in range(len(nulls)):    # commence 4 loop
+        missing = nulls[n]     # assign variable to nulls
+        ratio = missing / len(df.columns)   # assign variable to ratio
+        # assign a dictionary for your dataframe to accept
+        r_dict = {'nulls': missing,
+                  'null_ratio': round(ratio, 5),
+                  'null_percentage': f'{round(ratio * 100)}%'
+                 }
+        output.append(r_dict)   # add dictonaries to list
+
+    row_nulls = pd.DataFrame(output, index = df.index)    # design frame
+    row_nulls = row_nulls.sort_values('nulls', ascending = False)
+
+    return row_nulls    # return the dataframe
